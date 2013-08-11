25da3a84f51237505588dd3f6179c60ea0a933e3
Darrell Nicholas <Darrell@Darrells-MacBook-Pro.local>
2013-08-11 07:48:49 -0500

    Initial Commit

---
+++ QuickSchedule/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QuickSchedule
+//
+//  Created by Darrell Nicholas on 8/11/13.
+//  Copyright (c) 2013 Darrell Nicholas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
